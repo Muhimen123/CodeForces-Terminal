06e6099c3f4ee09c3af44c93bee8c634f886899c
Al Muhimen <almuhimen4@gmail.com>
2020-06-20 08:19:54 +0600

    Added advanced filter mode

--- contests.py
+++ contests.py
@@ -3,17 +3,174 @@ import colorama
 import json
 from tabulate import tabulate
 from datetime import datetime, timedelta
-
+from questions import con_questions
+import error_msg
 
 def contest_list(raw_cmd, adv=False):
     colorama.init()
     URL = 'https://codeforces.com/api/contest.list?'
 
     if 'true' in raw_cmd:
-        adv = True
+        answer = con_questions.contest_main_qus()
+        if answer['sort_by'].lower() == 'by_phases':
+            answer2 = con_questions.contest_phase_qus()
+        elif answer['sort_by'].lower() == 'by_divs':
+            answer2 = con_questions.contest_div_qus()
+        else:
+            answer2 = {'ans': 'Null'}
 
-    if adv == True:
-        print("Prompt for advanced commands")
+        if answer2['ans'] == 'Null':
+            try:
+                response = requests.get(URL)
+                if response.status_code != 200:
+                    print(colorama.Fore.RED + "Sorry, we recieved a " + colorama.Fore.RESET, end='')
+                    print(f"{colorama.Back.RED}{colorama.Fore.WHITE} {response.status_code} {colorama.Style.RESET_ALL}", end='')
+                    print(colorama.Fore.RED, "response code from CODEFORCES. 😟", colorama.Fore.RESET)
+                else:
+                    data = json.loads(response.text)
+                    contests_data= data['result']
+
+                    table_data = []
+                    headers = ['ID', 'NAME', 'TYPE', 'PHASE', 'START TIME', 'DURATION']
+
+                    if int(answer['contest_numbers']) > len(contests_data):
+                        answer['contest_numbers'] = len(contests_data)
+                    else:
+                        answer['contest_numbers'] = int(answer['contest_numbers'])
+
+                    for i in range(answer['contest_numbers']):
+                        ids = contests_data[i]['id']
+                        name = contests_data[i]['name']
+                        types = contests_data[i]['type']
+                        phase = contests_data[i]['phase']
+                        starttime = contests_data[i]['startTimeSeconds']
+                        duration = contests_data[i]['durationSeconds']
+
+                        if len(name) > 50:
+                            name = name[:47] + '...'
+
+                        starttime =  datetime.fromtimestamp(int(starttime))
+                        starttime = starttime.strftime("%Y-%m-%d  %H:%M")
+
+                        duration = timedelta(seconds=int(duration))
+
+                        row = [ids, name, types, phase, starttime, duration]
+                        table_data.append(row)
+
+                    table = tabulate(table_data, headers=headers, tablefmt='github')
+                    print(colorama.Fore.GREEN + table + colorama.Fore.RESET)
+
+            except Exception:
+                error_msg.error()
+
+        elif answer['sort_by'].lower() == 'by_divs':
+            try:
+                response = requests.get(URL)
+                if response.status_code != 200:
+                    print(colorama.Fore.RED + "Sorry, we recieved a " + colorama.Fore.RESET, end='')
+                    print(f"{colorama.Back.RED}{colorama.Fore.WHITE} {response.status_code} {colorama.Style.RESET_ALL}", end='')
+                    print(colorama.Fore.RED, "response code from CODEFORCES. 😟", colorama.Fore.RESET)
+                else:
+                    data = json.loads(response.text)
+                    contests_data= data['result']
+
+                    table_data = []
+                    headers = ['ID', 'NAME', 'TYPE', 'PHASE', 'START TIME', 'DURATION']
+
+                    if int(answer['contest_numbers']) > len(contests_data):
+                        answer['contest_numbers'] = len(contests_data)
+                    else:
+                        answer['contest_numbers'] = int(answer['contest_numbers'])
+
+                    contest_div_match = 0
+
+                    for i in range(len(contests_data)):
+                        if contest_div_match == answer['contest_numbers']:
+                            break
+
+                        if answer2['ans'].lower() in contests_data[i]['name'].lower():
+                            contest_div_match += 1
+
+                            ids = contests_data[i]['id']
+                            name = contests_data[i]['name']
+                            types = contests_data[i]['type']
+                            phase = contests_data[i]['phase']
+                            starttime = contests_data[i]['startTimeSeconds']
+                            duration = contests_data[i]['durationSeconds']
+
+                            if len(name) > 50:
+                                name = name[:47] + '...'
+
+                            starttime =  datetime.fromtimestamp(int(starttime))
+                            starttime = starttime.strftime("%Y-%m-%d  %H:%M")
+
+                            duration = timedelta(seconds=int(duration))
+
+                            row = [ids, name, types, phase, starttime, duration]
+                            table_data.append(row)
+                        else:
+                            pass
+
+                    table = tabulate(table_data, headers=headers, tablefmt='github')
+                    print(colorama.Fore.GREEN + table + colorama.Fore.RESET)
+
+            except Exception:
+                error_msg.error()
+
+        elif answer['sort_by'].lower() == 'by_phases':
+            try:
+                response = requests.get(URL)
+                if response.status_code != 200:
+                    print(colorama.Fore.RED + "Sorry, we recieved a " + colorama.Fore.RESET, end='')
+                    print(f"{colorama.Back.RED}{colorama.Fore.WHITE} {response.status_code} {colorama.Style.RESET_ALL}", end='')
+                    print(colorama.Fore.RED, "response code from CODEFORCES. 😟", colorama.Fore.RESET)
+                else:
+                    data = json.loads(response.text)
+                    contests_data= data['result']
+
+                    table_data = []
+                    headers = ['ID', 'NAME', 'TYPE', 'PHASE', 'START TIME', 'DURATION']
+
+                    if int(answer['contest_numbers']) > len(contests_data):
+                        answer['contest_numbers'] = len(contests_data)
+                    else:
+                        answer['contest_numbers'] = int(answer['contest_numbers'])
+
+                    contest_div_match = 0
+
+                    for i in range(len(contests_data)):
+                        if contest_div_match == answer['contest_numbers']:
+                            break
+
+                        if answer2['ans'].lower() in contests_data[i]['phase'].lower():
+                            contest_div_match += 1
+
+                            ids = contests_data[i]['id']
+                            name = contests_data[i]['name']
+                            types = contests_data[i]['type']
+                            phase = contests_data[i]['phase']
+                            starttime = contests_data[i]['startTimeSeconds']
+                            duration = contests_data[i]['durationSeconds']
+
+                            if len(name) > 50:
+                                name = name[:47] + '...'
+
+                            starttime =  datetime.fromtimestamp(int(starttime))
+                            starttime = starttime.strftime("%Y-%m-%d  %H:%M")
+
+                            duration = timedelta(seconds=int(duration))
+
+                            row = [ids, name, types, phase, starttime, duration]
+                            table_data.append(row)
+                        else:
+                            pass
+
+                    table = tabulate(table_data, headers=headers, tablefmt='github')
+                    print(colorama.Fore.GREEN + table + colorama.Fore.RESET)
+
+            except Exception:
+                error_msg.error()
+
     else:
         try:
             response = requests.get(URL)
@@ -36,6 +193,9 @@ def contest_list(raw_cmd, adv=False):
                     starttime = contests_data[i]['startTimeSeconds']
                     duration = contests_data[i]['durationSeconds']
 
+                    if len(name) > 50:
+                        name = name[:47] + '...'
+
                     starttime =  datetime.fromtimestamp(int(starttime))
                     starttime = starttime.strftime("%Y-%m-%d  %H:%M")
 
@@ -48,18 +208,18 @@ def contest_list(raw_cmd, adv=False):
                 print(colorama.Fore.GREEN + table + colorama.Fore.RESET)
 
         except Exception:
-            print(colorama.Fore.RED + "Please check your internet connection and then try", colorama.Fore.RESET)
+                error_msg.error()
 
     print(colorama.Style.RESET_ALL)
 
 
-def contest_standings(adv=False):
+def contest_standings(raw_cmd, adv=False):
     pass
 
 
-def contest_ratings_change(adv=False):
+def contest_ratings_change(raw_cmd, adv=False):
     pass
 
 
-def contest_info(contest_id):
+def contest_problems(contest_id):
     pass
